93da11610e75eda97e8495de8440a92aed14c3a5
Nithu Roban <mca2241@rajagiri.edu>
2023-02-06 15:47:14 +0530

    first commit

---
+++ form.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<form action="welcome1.php" method="get">
+Name: <input type="text" name="name"><br><br>
+E-mail: <input type="text" name="email"><br><br>
+Contact: <input type="text" name="phone"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
---
+++ welcome1.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"];?><br>
+Your contact  number is:<?php  echo $_GET["phone"]; ?>
+
+</body>
+</html>
